8637e6e29eaadd0d46722b3603275f2725bf2fd5
mconnors333 <matthew.connors333@gmail.com>
2023-01-24 19:58:57 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
-// import { TopBarComponent } from './top-bar/top-bar.component';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
