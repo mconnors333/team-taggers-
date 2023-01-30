690b7a9d6bf83dbdaae6b592cf8c7ee4aec3e6d8
mconnors333 <matthew.connors333@gmail.com>
2023-01-29 22:13:53 -0500

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
-export const firebase = {
-  apiKey: 'AIzaSyDyEPGgy_yp-g1Qv976rPMDmHhZhweXVt4',
-  authDomain: 'team-taggers.firebaseapp.com',
-  projectId: 'team-taggers',
-  storageBucket: 'team-taggers.appspot.com',
-  messagingSenderId: '927258496672',
-  appId: '1:927258496672:web:80b8fd6173bde0da1683c5',
-  measurementId: 'G-ZKQ71KHQYR',
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDyEPGgy_yp-g1Qv976rPMDmHhZhweXVt4',
+    authDomain: 'team-taggers.firebaseapp.com',
+    projectId: 'team-taggers',
+    storageBucket: 'team-taggers.appspot.com',
+    messagingSenderId: '927258496672',
+    appId: '1:927258496672:web:80b8fd6173bde0da1683c5',
+    measurementId: 'G-ZKQ71KHQYR',
+  },
 };
